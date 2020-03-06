b0e99329a08cea76b6bc2baec3f09e47cb284557
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-06 18:10:40 +0000

    Done.

---
+++ countdown.rb
@@ -0,0 +1,16 @@
+def countdown(n)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+   "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  sleep(5)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
